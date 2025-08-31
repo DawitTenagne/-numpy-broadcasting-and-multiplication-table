254750ffa0dff015b04fbf63b4b6145e4cd0cc5d
DawitTenagne <dawit0038@gmail.com>
2025-08-31 11:48:47 -0500

    filtering using numpy

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import numpy as np
+#---------------------------------------------------------
+ages = np.array([[21,17,65,16,19,33,24,18,33],
+                 [15,21,26,99,64,31,17,25,27]])
 
-array1 = np.array([[1,2,3,4,5,6,7,8,9,10]])
-array2 = np.array([[1],[2],[3],[4],[5],[6],[7],[8],[9],[10]])
+teenagers = ages[ages < 18]
+adults = ages[(ages>=18) & (ages < 65)]
+not_adults = ages[(ages<18) | (ages >=65)]
+seniors = ages[ages>=65]
+evens = ages[ages % 2 == 0]
+odds = ages[ages % 2 != 0]
+#--------------------------------------------------------
+print(teenagers)
+print(adults)
+print(not_adults)
+print(seniors)
+print(evens)
+print(odds)
 
-print(array1.shape)
-print(array2.shape)
-print ( array1 * array2)
 
 
 
