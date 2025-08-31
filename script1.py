7b6aae1e7f3a48bf606073e92dfe3d4df56cc539
DawitTenagne <dawit0038@gmail.com>
2025-08-31 06:36:28 -0500

    Using numpy to implement the concept of broadcasting

---
+++ script1.py
@@ -0,0 +1,11 @@
+
+
+def favourite_food(food):
+    print(f"your favourite food is pizza")
+
+def main():
+    print("script1 is running directly")
+    favourite_food("doro wet")
+
+if __name__ == '__main__':
+    main()
---
+++ script2.py
@@ -0,0 +1,3 @@
+from script1 import *
+print("this is script2 message")
+favourite_food("burger")
